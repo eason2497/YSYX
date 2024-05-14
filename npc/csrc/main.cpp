a51090694244a3c9465413abda7b65d56a3483e0
WhatMakesMeFun <eason2497@gmail.com>
2024-05-14 14:36:07 +0800

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels bb725d6f8223dd7de831c3b692e8c4531e9d01af kernels,demo,donut: only clear screen before drawing

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,28 +1,26 @@
 #include <nvboard.h>
-//#include <Vtop.h>
-#include <Vlight.h>
-
+#include <Vtop.h>
+#include "verilated.h"
 
 static TOP_NAME dut;
 
-void nvboard_bind_all_pins(Vlight* light);
+void nvboard_bind_all_pins(Vtop* top);
 
 void single_cycle() {
 	dut.clk = 0; dut.eval();
 	dut.clk = 1; dut.eval();
+	//dut.eval();
 }
 
-void reset(int n) {
-	dut.rst = 1;
-	while (n -- > 0) single_cycle();
-	dut.rst = 0;
+static void reset(int n) {
+  dut.rst = 1;
+  while (n -- > 0) single_cycle();
+  dut.rst = 0;
 }
 
-
-int main() {
+int main(int argc, char** argv) {
 	nvboard_bind_all_pins(&dut);
 	nvboard_init();
-
 	reset(10);
 
 	while(1) {
